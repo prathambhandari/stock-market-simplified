6745db9c70aa5b941ed7ce5e7f7197bff609b960
prathambhandari <prathamkrshnabhandari@gmail.com>
2025-01-25 00:04:22 +0530

    update quick browse section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import About from "./Components/About";
 import HeroSection from "./Components/HeroSection";
 import LearnSection from "./Components/LearnSection";
@@ -18,8 +19,10 @@ export default function Home() {
         <RiBookOpenFill className=" h-12 w-12  text-primary mb-4" />
         <div className=" font-normal tracking-wide text-neutral-400 leading-normal  flex flex-col items-center justify-center ">
           <h1 className="text-lg font-medium mb-2 text-neutral-300 text-center">Ready to Start Building Wealth Today?</h1>
-          <h1 className="text-base font-normal mb-6 text-neutral-400 text-center leading-relaxed "> We’re confident you’ll love the course, but if you’re not satisfied within the first 30 days, we’ll offer you a upcoming courses for free, no questions asked.</h1>
+          <h1 className="text-base font-normal mb-6 text-neutral-400 text-center leading-relaxed "> </h1>
+          <Link href="https://course.prathambhandari.com/">
           <button className="w-fit bg-primary text-neutral-800 px-4 py-3 text-base rounded-xl font-medium">Quick Browse</button>
+          </Link>
         </div>
 
       </div>
