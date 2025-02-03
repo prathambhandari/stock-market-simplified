4c6a184846e010654f03cb9edad2f5e3f3ac7654
prathambhandari <prathamkrshnabhandari@gmail.com>
2025-02-04 01:32:01 +0530

    Added coupon section

--- app/Components/HeroSection.js
+++ app/Components/HeroSection.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 export default function HeroSection() {
-    const [timeLeft, setTimeLeft] = useState(42 * 60);
+    const [timeLeft, setTimeLeft] = useState(12 * 60);
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
@@ -44,7 +44,7 @@ export default function HeroSection() {
         <div className="*:rounded-2xl *:p-5 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 z-50">
             <div className="bg-noise bg-primary flex flex-col items-start relative col-span-1 sm:col-span-1 md:col-span-1 lg:col-span-3 xl:col-span-3 drop-shadow h-fit overflow-hidden group ease-in-out duration-300">
                 {/* Image Section */}
-                <div
+             {/*  <div
                     className="relative w-full h-48 lg:h-[50vh] overflow-hidden pb-8 bg-black rounded-2xl"
                     style={{
                         backgroundImage: "url('/assets/thumbnail.png')",
@@ -53,6 +53,7 @@ export default function HeroSection() {
                         backgroundRepeat: "no-repeat",
                     }}
                 ></div>
+                 */} 
 
                 <div className="*:z-20 relative">
                     <h1 className="text-2xl font-semibold text-neutral-900 leading-normal mb-2 font-mono pt-8">
@@ -71,6 +72,13 @@ export default function HeroSection() {
                             * Save 94% Off Competitor Pricing.{" "}
                         </h6>
 
+                        <h1 className="text-l font-medium text-neutral-900 leading-normal mb-2 font-mono ">
+                        🎉 Early Bird Offer! First 50 learners get the course for just ₹198 (instead of ₹449)!
+                          </h1>
+                          <h1 className="text-l font-medium text-neutral-900 leading-normal mb-2 font-mono pb-8">
+                        🏷️ Use Code: "<span className="font-bold font-sans"> FIRST50 </span>" at Checkout. Hurry! Limited Spots!
+                          </h1>
+
                         <div className="bg-black bg-opacity-60  text-white p-2  animate-bounce rounded-lg max-w-[220px] text-center">
                             <p className="text-sm font-medium tracking-wide ">
                                 Hurry! Offer ends in:{" "}
@@ -78,6 +86,7 @@ export default function HeroSection() {
                                 {String(seconds).padStart(2, "0")}
                             </p>
                         </div>
+                        
                         <div className="pb-4 "></div>
                         <Link href="https://course.prathambhandari.com/">
                             <button className="w-full sm:w-full md:w-fit lg:w-fit xl:w-fit relative overflow-hidden dark:bg-neutral-950 bg-white dark:text-white text-neutral-950 rounded-lg px-4 py-3 text-base font-normal tracking-wide transition-all duration-75 ease-in-out drop-shadow hover:drop-shadow-md hover:scale-105 group transform-gpu">
